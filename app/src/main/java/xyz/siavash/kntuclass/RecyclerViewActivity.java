60a74211e43f5a66f7a553d548e1e49283d2845c
siavash abdoli <siavash.abdoli@gmail.com>
2018-11-03 18:55:21 +0330

    set recyclerView for horizontal mode

--- app/src/main/java/xyz/siavash/kntuclass/RecyclerViewActivity.java
+++ app/src/main/java/xyz/siavash/kntuclass/RecyclerViewActivity.java
@@ -2,6 +2,7 @@ package xyz.siavash.kntuclass;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
@@ -18,12 +19,13 @@ public class RecyclerViewActivity extends AppCompatActivity {
     recyclerView = findViewById(R.id.recycler_view);
     MyAdapter myAdapter = new MyAdapter();
     recyclerView.setAdapter(myAdapter);
-    recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    RecyclerView.LayoutManager lm = new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL, true);
+    recyclerView.setLayoutManager(lm);
 
     List<GoodsItem> items = new ArrayList<>();
     for (int i = 0; i < 15; i++) {
       GoodsItem goodsItem = new GoodsItem(String.valueOf(4000 * (i + 1)),
-          String.valueOf(3000 * (i + 1)) , String.format("title%s" , i),
+          String.valueOf(3000 * (i + 1)), String.format("title%s", i),
           "https://dkstatics-public.digikala.com/digikala-products/631659.jpg?x-oss-process=image/resize,m_lfit,h_600,w_600/quality,q_80");
       items.add(goodsItem);
     }
