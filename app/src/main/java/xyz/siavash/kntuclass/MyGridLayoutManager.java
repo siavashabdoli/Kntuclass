f5d4242f5759cb3244817c5410c60cbc2e3bcc13
siavash abdoli <siavash.abdoli@gmail.com>
2018-11-24 18:14:52 +0330

    fix layout for recyclerview

---
+++ app/src/main/java/xyz/siavash/kntuclass/MyGridLayoutManager.java
@@ -0,0 +1,19 @@
+package xyz.siavash.kntuclass;
+
+import android.content.Context;
+import android.support.v7.widget.GridLayoutManager;
+import android.util.AttributeSet;
+
+class MyGridLayoutManager extends GridLayoutManager {
+  public MyGridLayoutManager(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+    super(context, attrs, defStyleAttr, defStyleRes);
+  }
+
+  public MyGridLayoutManager(Context context, int spanCount) {
+    super(context, spanCount);
+  }
+
+  public MyGridLayoutManager(Context context, int spanCount, int orientation, boolean reverseLayout) {
+    super(context, spanCount, orientation, reverseLayout);
+  }
+}
