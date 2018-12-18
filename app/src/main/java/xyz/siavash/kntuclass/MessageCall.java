fc10ace88d7d2e4e1a7ba26ad027b7318b7b52e5
Morteza Taghaddomi Masoumi <mortezataqaddomi99@gmail.com>
2018-12-18 22:23:01 +0330

    gson apiprovider HTTP request to Siavash server

---
+++ app/src/main/java/xyz/siavash/kntuclass/MessageCall.java
@@ -0,0 +1,14 @@
+package xyz.siavash.kntuclass;
+
+public class MessageCall {
+  private String message;
+
+
+  public String getMessage() {
+    return message;
+  }
+
+  public MessageCall(String message) {
+    this.message = message;
+  }
+}
